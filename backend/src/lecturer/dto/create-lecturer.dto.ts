e3cd1a1f2b5acc1736b3b1c31c893f3372b5e3f0
Marcel <marcelino.sungkono@binus.ac.id>
2026-05-10 20:00:15 +0700

    create  lecturer dto

--- backend/src/lecturer/dto/create-lecturer.dto.ts
+++ backend/src/lecturer/dto/create-lecturer.dto.ts
@@ -1,1 +1,20 @@
-export class CreateLecturerDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateLecturerDto {
+  @IsString()
+  @IsNotEmpty()
+  lecturerId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password!: string;
+}
