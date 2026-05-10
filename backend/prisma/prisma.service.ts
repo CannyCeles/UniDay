6e509398f8e139c373acf7796bba6ebf53d45335
Marcel <marcelino.sungkono@binus.ac.id>
2026-05-10 19:05:28 +0700

    prisma.service.ts mariadb adapter

---
+++ backend/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable} from '@nestjs/common';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from 'generated/prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient {
+  constructor() {
+    const port = parseInt(process.env.DB_PORT ?? '3306');
+    
+    const adapter = new PrismaMariaDb({
+      host: process.env.DB_HOST ?? 'localhost',
+      port: port,
+      user: process.env.DB_USERNAME ?? 'root',
+      password: process.env.DB_PASSWORD ?? '',
+      database: process.env.DB_NAME ?? 'uniday_db',
+    });
+
+    super({ adapter : adapter});
+  }
+}
