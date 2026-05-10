33b8415baa486c3b322f7a7d816154ce4858c0e4
Marcel <marcelino.sungkono@binus.ac.id>
2026-05-10 19:59:44 +0700

    create class session dto

--- backend/src/class-session/dto/create-class-session.dto.ts
+++ backend/src/class-session/dto/create-class-session.dto.ts
@@ -1,1 +1,15 @@
-export class CreateClassSessionDto {}
+import { IsNotEmpty, IsNumber, IsDateString } from 'class-validator';
+
+export class CreateClassSessionDto {
+  @IsNumber()
+  @IsNotEmpty()
+  courseId!: number;
+
+  @IsDateString()
+  @IsNotEmpty()
+  startTime!: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  endTime!: string;
+}
