8ba79666543ca9abf78ade3b6900e24daeca0502
Marcel <marcelino.sungkono@binus.ac.id>
2026-05-11 16:46:08 +0700

    login register controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,34 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateStudentDto } from '../student/dto/create-student.dto';
+import { CreateLecturerDto } from '../lecturer/dto/create-lecturer.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @Post('register/student')
+  registerStudent(@Body() createStudentDto: CreateStudentDto) {
+    return this.authService.registerStudent(createStudentDto);
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
+  @Post('register/lecturer')
+  registerLecturer(@Body() createLecturerDto: CreateLecturerDto) {
+    return this.authService.registerLecturer(createLecturerDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 }
