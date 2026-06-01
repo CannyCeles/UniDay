0c2d80c8f063d19536f6e41b57607a70832f03db
Marcel <marcelino.sungkono@binus.ac.id>
2026-06-01 22:55:16 +0700

    fix: added back maints

---
+++ backend/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: [
+      'https://uni-day.vercel.app',
+      'http://localhost:5173'
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+
+  app.useStaticAssets(join(process.cwd(), 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
