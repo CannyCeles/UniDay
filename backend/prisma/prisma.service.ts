0d500450954c5ec20feae8c24aa0a61a14aef72b
Marcel <marcelino.sungkono@binus.ac.id>
2026-06-02 09:08:51 +0700

    fix(backend): native prisma client maria db

--- backend/prisma/prisma.service.ts
+++ backend/prisma/prisma.service.ts
@@ -1,23 +1,9 @@
-import { Injectable} from '@nestjs/common';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient {
+export class PrismaService extends PrismaClient {
   constructor() {
-    const port = parseInt(process.env.DB_PORT ?? '3306');
-    const isCloud = process.env.DB_HOST && process.env.DB_HOST !== 'localhost' && process.env.DB_HOST !== '127.0.0.1';
-    
-    const adapter = new PrismaMariaDb({
-      host: process.env.DB_HOST ?? 'localhost',
-      port: port,
-      user: process.env.DB_USERNAME ?? 'root',
-      password: process.env.DB_PASSWORD ?? '',
-      database: process.env.DB_NAME ?? 'uniday_db',
-      ssl: isCloud ? { rejectUnauthorized: false } : undefined,
-    });
-
-    super({ adapter : adapter});
+    super();
   }
 }
